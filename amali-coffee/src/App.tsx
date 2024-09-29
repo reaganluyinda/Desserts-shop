4ecd9bea223c82f0dd16280680faae0948d44db4
reagan luyinda <luyindareagan@gmail.com>
2024-09-29 08:34:02 +0300

    Updated App.tsx
    added new imports of components

--- amali-coffee/src/App.tsx
+++ amali-coffee/src/App.tsx
@@ -1,9 +1,19 @@
+import Cart from "./Cart";
 import Header from "./Header";
+import Shop from "./shop";
 
 function App() {
   return (
     <>
-      <Header />
+      <div className="min-h-screen bg-slate-100 px-20 pt-10">
+        <Header />
+        <div className="flex">
+          <Shop />
+          <div className="flex-1">
+            <Cart />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
