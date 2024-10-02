82059689199fd2b0aafafc00eff88c113e861e52
reagan luyinda <luyindareagan@gmail.com>
2024-10-02 12:56:12 +0300

    updated the shop.tsx
    added more grid elements to make it more responsive

--- amali-coffee/src/Shop.tsx
+++ amali-coffee/src/Shop.tsx
@@ -5,9 +5,12 @@ import macaron from "./assets/image-macaron-desktop.jpg";
 import tiramisu from "./assets/image-tiramisu-desktop.jpg";
 import baklva from "./assets/image-baklava-desktop.jpg";
 import pie from "./assets/image-meringue-desktop.jpg";
+import cake from "./assets/image-cake-desktop.jpg";
+import brownie from "./assets/image-brownie-desktop.jpg";
+import cotta from "./assets/image-panna-cotta-desktop.jpg";
 function Shop() {
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
       <Card
         waffleimage={waffleimage}
         product="Waffle"
@@ -43,6 +46,27 @@ function Shop() {
         product="Pie"
         productdefn="Lemon Meringue Pie"
         price={5.1}
+      />
+
+      <Card
+        waffleimage={cake}
+        product="Cake"
+        productdefn="Red Velvet Cake"
+        price={4.5}
+      />
+
+      <Card
+        waffleimage={brownie}
+        product="Brownie"
+        productdefn="Salted Caramel Brownie"
+        price={5.5}
+      />
+
+      <Card
+        waffleimage={cotta}
+        product="Panna cotta"
+        productdefn="Vanilla Panna Cotta"
+        price={6.5}
       />
     </div>
   );
