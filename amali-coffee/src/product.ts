71990fe44cb9060bbc1ee1a11ce440071bade3e9
reagan luyinda <luyindareagan@gmail.com>
2024-10-03 10:51:52 +0300

    feat; Created the products.ts
    created the products file having all products in an object

---
+++ amali-coffee/src/product.ts
@@ -0,0 +1,74 @@
+import waffleimage from "./assets/image-waffle-desktop.jpg";
+import cremebrulee from "./assets/image-creme-brulee-desktop.jpg";
+import macaron from "./assets/image-macaron-desktop.jpg";
+import tiramisu from "./assets/image-tiramisu-desktop.jpg";
+import baklva from "./assets/image-baklava-desktop.jpg";
+import pie from "./assets/image-meringue-desktop.jpg";
+import cake from "./assets/image-cake-desktop.jpg";
+import brownie from "./assets/image-brownie-desktop.jpg";
+import cotta from "./assets/image-panna-cotta-desktop.jpg";
+export const products = [
+  {
+    id: 1,
+    productimage: waffleimage,
+    product: "Waffle",
+    productdefn: "Waffle with berries",
+    price: 6.5,
+  },
+  {
+    id: 2,
+    productimage: cremebrulee,
+    product: "Creme brulee",
+    productdefn: "Vanilla Bean Creme Brulee",
+    price: 7.2,
+  },
+  {
+    id: 3,
+    productimage: macaron,
+    product: "Macaron",
+    productdefn: "Macaron Mix of Five",
+    price: 8.3,
+  },
+  {
+    id: 4,
+    productimage: tiramisu,
+    product: "Tiramisu",
+    productdefn: "Classic Tiramisu",
+    price: 5.5,
+  },
+  {
+    id: 5,
+    productimage: baklva,
+    product: "Baklva",
+    productdefn: "Pistachio Baklva",
+    price: 4.2,
+  },
+  {
+    id: 6,
+    productimage: pie,
+    product: "Pie",
+    productdefn: "Lemon Meringue Pie",
+    price: 5.1,
+  },
+  {
+    id: 7,
+    productimage: cake,
+    product: "Cake",
+    productdefn: "Red Velvet Cake",
+    price: 4.5,
+  },
+  {
+    id: 8,
+    productimage: brownie,
+    product: "Brownie",
+    productdefn: "Salted Caramel Brownie",
+    price: 5.5,
+  },
+  {
+    id: 9,
+    productimage: cotta,
+    product: "Panna cotta",
+    productdefn: "Vanilla Panna Cotta",
+    price: 6.5,
+  },
+];
