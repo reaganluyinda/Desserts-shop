a172960f610fe816d3d47195046d2e353610aad7
reagan luyinda <luyindareagan@gmail.com>
2024-10-16 18:46:54 +0300

    ref; updated the App.tsx
    imported the context provider and wrapped it around the components imported in the App.tsx

--- amali-coffee/src/App.tsx
+++ amali-coffee/src/App.tsx
@@ -1,4 +1,5 @@
 import Cart from "./Cart";
+import ShopContextProvider from "./context/shop-context";
 import Header from "./Header";
 import Shop from "./Shop";
 
@@ -6,13 +7,15 @@ function App() {
   return (
     <>
       <div className="min-h-screen bg-slate-100 px-20 pt-10">
-        <Header />
-        <div className="flex flex-wrap ">
-          <Shop />
-          <div className="flex-1">
-            <Cart />
+        <ShopContextProvider>
+          <Header />
+          <div className="flex flex-wrap ">
+            <Shop />
+            <div className="flex-1">
+              <Cart />
+            </div>
           </div>
-        </div>
+        </ShopContextProvider>
       </div>
     </>
   );
