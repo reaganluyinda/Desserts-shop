f4865f32a18c98bca1735d3e22b63b10b082117f
reagan luyinda <luyindareagan@gmail.com>
2024-10-16 18:41:39 +0300

    ref; updated the Card.tsx
    added the  increment and decrement buttons on the card to add products to the Cart

--- amali-coffee/src/Card.tsx
+++ amali-coffee/src/Card.tsx
@@ -1,12 +1,26 @@
+import { useContext } from "react";
 import cartimage from "./assets/icon-add-to-cart.svg";
-interface CardProps {
+import ShopContext from "./context/ShopContext";
+
+type CardProps = {
   id: number;
   productimage: string;
   product: string;
   productdefn: string;
   price: number;
-}
-function Card({ id, productimage, product, productdefn, price }: CardProps) {
+};
+const Card: React.FC<CardProps> = ({
+  id,
+  productimage,
+  product,
+  productdefn,
+  price,
+}) => {
+  const { addToCart, removeFromCart, cartItems, inCart } =
+    useContext(ShopContext);
+
+  const cartItemQuantity = cartItems[id];
+
   return (
     <div className="border-1 border-b-gray-600 rounded-lg max-w-64 mr-4 mt-4">
       <div className="flex-col flex items-center justify-center relative">
@@ -15,10 +29,33 @@ function Card({ id, productimage, product, productdefn, price }: CardProps) {
           src={productimage}
           alt="waffles"
         ></img>
-        <button className=" bg-slate-50 font-semibold border-2 px-4 py-2 rounded-full absolute bottom-0 translate-y-1/2 hover:bg-slate-200 flex gap-2 ">
-          <img className="" src={cartimage} alt="cart image"></img>
-          Add to Cart
-        </button>
+        {cartItemQuantity === 0 ? (
+          <button
+            onClick={() => addToCart(id)}
+            className=" bg-slate-50 font-semibold border-2 px-4 py-2 rounded-full absolute bottom-0 translate-y-1/2 hover:bg-slate-200 flex gap-2 "
+          >
+            <img className="" src={cartimage} alt="cart image"></img>
+            Add to Cart
+          </button>
+        ) : (
+          <div className="flex justify-center space-x-8 bg-orange-700 border-2 px-2 py-2 rounded-full absolute bottom-0 translate-y-1/2">
+            <button
+              className="  hover:bg-white text-white hover:text-orange-700 border-2 font-semibold rounded-full px-2 py-0 flex justify-center"
+              onClick={() => removeFromCart(id)}
+            >
+              -
+            </button>
+            <span className=" text-white font-semibold">
+              {cartItemQuantity}
+            </span>
+            <button
+              className=" hover:bg-white text-white hover:text-orange-700 border-2 font-semibold  rounded-full px-[6px] py-0"
+              onClick={() => addToCart(id)}
+            >
+              +
+            </button>
+          </div>
+        )}
       </div>
       <div className="py-5">
         <h1>{product}</h1>
@@ -27,6 +64,6 @@ function Card({ id, productimage, product, productdefn, price }: CardProps) {
       </div>
     </div>
   );
-}
+};
 
 export default Card;
