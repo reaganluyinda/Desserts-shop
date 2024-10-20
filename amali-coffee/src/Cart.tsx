9cdffa95868617d382b8c91ef6696b03b18cdf10
reagan luyinda <luyindareagan@gmail.com>
2024-10-20 12:30:12 +0300

    updated Cartitem and Cart
    updated the cartitem to be displayed in the cart when a product is selected

--- amali-coffee/src/Cart.tsx
+++ amali-coffee/src/Cart.tsx
@@ -1,44 +1,72 @@
 import emptycart from "./assets/illustration-empty-cart.svg";
+import carbon_neutral from "./assets/icon-carbon-neutral.svg";
 import { products } from "./product";
-import { useContext } from "react";
-import ShopContext from "./context/ShopContext";
 import CartItem from "./CartItem";
+import { useShopContext } from "./context/shop-context";
 function Cart() {
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount, setShowModal } = useShopContext();
+
+  const totalAmount = getTotalCartAmount();
+
+  const totalItems = Object.values(cartItems).reduce(
+    (sum, quantity) => sum + quantity,
+    0
+  );
 
   return (
     <div className="border-2 border-solid rounded-xl bg-slate-50 min-h-[350px] mx-4 pb-4">
       <h1 className="text-orange-600 font-bold px-5 pt-5 text-[25px]">
-        Your cart (0)
+        Your cart
+        {totalItems > 0 && <span className="px-1">({totalItems})</span>}
       </h1>
-      {/* <div className="flex items-center justify-center p-8">
-        <img src={emptycart} alt="empty cart"></img>
-      </div>
-      <div>
-        <p className="font-semibold text-center py-4">
-          Your added items will appear here
-        </p>
-        </div> */}
-      <div>
-        {products.map((product) =>
-          cartItems[product.id] !== 0 ? (
-            <CartItem key={product.id} {...product} />
-          ) : null
-        )}
-      </div>
-      <div className="space-y-3">
-        <div className="mx-4 py-3">
-          <p>Order Total</p>
+
+      {totalAmount > 0 ? (
+        <div>
+          <div>
+            {products.map((product) =>
+              cartItems[product.id] !== 0 ? (
+                <CartItem
+                  key={product.id}
+                  {...product}
+                  quantity={cartItems[product.id]}
+                />
+              ) : null
+            )}
+          </div>
+          <div className="space-y-3">
+            <div className="mx-4 py-3 flex items-center">
+              <p className="flex-1">Order Total</p>
+              <p className="font-bold text-xl">${totalAmount.toFixed(2)}</p>
+            </div>
+            <div className="bg-pink-100 mx-4 text-center p-4 rounded-lg flex justify-center items-center gap-2">
+              <img src={carbon_neutral} alt="Carbon-neutral"></img>
+              <p>
+                This is a <span className="font-semibold">carbon-neutral</span>{" "}
+                delivery
+              </p>
+            </div>
+            <div className="flex justify-center items-center ">
+              <button
+                onClick={() => setShowModal(true)}
+                className="bg-orange-800 rounded-full px-4 p-2 text-white font-semibold w-full mx-4"
+              >
+                Confirm order
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="bg-green-300 mx-4 text-center p-4 rounded-lg">
-          <p>This is a carbon-neutral delivery</p>
+      ) : (
+        <div>
+          <div className="flex items-center justify-center p-8">
+            <img src={emptycart} alt="empty cart"></img>
+          </div>
+          <div>
+            <p className="font-semibold text-center py-4">
+              Your added items will appear here
+            </p>
+          </div>
         </div>
-        <div className="flex justify-center items-center ">
-          <button className="bg-orange-700 rounded-full px-4 p-2 text-white font-semibold w-full mx-4">
-            Confirm order
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- amali-coffee/src/CartItem.tsx
+++ amali-coffee/src/CartItem.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { CircleX } from "lucide-react";
+import { useShopContext } from "./context/shop-context";
 
 type CardProps = {
   id: number;
@@ -7,16 +7,35 @@ type CardProps = {
   product: string;
   productdefn: string;
   price: number;
+  quantity: number;
 };
-const CartItem: React.FC<CardProps> = ({ productdefn, price }) => {
+const CartItem: React.FC<CardProps> = ({
+  id,
+  productdefn,
+  price,
+  quantity,
+}) => {
+  const { deleteFromCart } = useShopContext();
+
+  const itemTotalPrice = price * quantity;
+
   return (
     <div className="px-4 flex items-center">
       <div className="border-b-2 p-2 flex-1">
         <p className="font-semibold">{productdefn}</p>
-        <p>${price}</p>
-      </div>
-      <div>
-        <CircleX />
+        <div className="flex items-center ">
+          <div className=" flex flex-1 gap-2">
+            <p className="text-orange-700 font-semibold">{quantity}x</p>
+            <p className="text-orange-800">@${price}</p>
+            <p className="text-orange-800">${itemTotalPrice.toFixed(2)}</p>
+          </div>
+          <div>
+            <CircleX
+              onClick={() => deleteFromCart(id)}
+              className="text-orange-600 hover:text-orange-700"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
